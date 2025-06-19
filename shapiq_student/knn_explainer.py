Fixed calculation bugs Changes: - np.abs -> len (error, we use the amount of neighbors) - self.labels -> y_minus_i (instead of using the whole dataset, we use the subset otherwise the indicies wouldn't align) - optimize calculations (don't calculate A1 and A2 if c<2)

shapiq_student/knn_explainer.py
@@ -17,7 +17,9 @@ class KNNExplainer(Explainer):
             intercation_values = self.knn_shapley(x)
         elif self.method == 'threshold':
             threshold = kwargs['threshold']
-            intercation_values = self.threshold_knn_shapley(x, threshold)
+            x_query = kwargs['x_query']
+            num_classes = kwargs['num_classes']
+            interaction_values = self.threshold_knn_shapley(x_query, threshold, num_classes)
         elif self.method == 'weighted':
             gamma = kwargs['gamma']
             interaction_values = self.weighted_knn_shapley(x,gamma)
@@ -31,9 +33,8 @@ class KNNExplainer(Explainer):
         # TODO Implement knn shapley
         pass
 
-
-    def threshold_knn_shapley(self, x_query: tuple[float,float], threshold: float, num_classes: int) -> np.ndarray:
-        """Berechnet die Threshold-KNN-Shapey Werte.
+    def threshold_knn_shapley(self, x_query: tuple[float, float], threshold: float, num_classes: int) -> np.ndarray:
+        """Berechnet die Threshold-KNN-Shapey Werte für einen Validierungspunkt.
 
         Args:
             x_query : Die Validierungspunkte (x_val, y_val).
@@ -43,59 +44,57 @@ class KNNExplainer(Explainer):
         Returns:
             np.ndarry: Ein Array mit den berechneten Spaley-Werten.
         """
-        # TODO Implement threshold
-        # Berechnet Treshold knn shapley werte fuer einen Validierungspunkt
-
         x_val, y_val = x_query
         X, y = self.dataset, self.labels
+        N = self.dataset.shape[0]  # Menge der Trainingspunkte
 
-        N = self.dataset.shape[0] # Menge der Trainingspunkte
-
-        # Initialisierung aller Shapley werte mit 0
+        # Initialisierung
         phi = np.zeros(N)
-
 
         for i in range(N):
             x_i, y_i = X[i], y[i]
 
-
-            #Trainingspunkte i aus den Daten entfernen
+            # Trainingspunkte i aus den Daten entfernen
             X_minus_i = np.delete(X, i, axis=0)
-            y_minus_i = np.delete(y, i ,axis=0)
+            y_minus_i = np.delete(y, i, axis=0)
 
             distance = np.linalg.norm(x_i - x_val)
-            neighbors_mask = distance <= threshold # if true
-            neighbor_indices = np.where(neighbors_mask)[0] # Indizes der Punkte innerhalb treshold / Labelvergleich
-
-            if len(neighbor_indices) > 0:
-                #Grösse des Reduzierten Trainingsdatensatz
+            if distance <= threshold:
+                # Grösse des Reduzierten Trainingsdatensatz
                 c = N - 1
 
                 distance_d_minus_zi = np.linalg.norm(X_minus_i - x_val, axis=1)
                 neighbor_indices_minus_zi = np.where(distance_d_minus_zi <= threshold)[0]
 
-                #Anzahl der Nachbarn von x_val in D-zi innerhalb des treshholds
-                c_x = 1 + np.abs(neighbor_indices_minus_zi)
+                # Anzahl der Nachbarn von x_val in D-zi innerhalb des treshholds
+                c_x = len(neighbor_indices_minus_zi) + 1
 
-                # Anzahl der Nachbarn von x_val in D-zi, mit gleiches Label haben
-                y_neighbors = self.labels[neighbor_indices_minus_zi]
-                c_plus = np.sum(y_neighbors == y_val)
+                term2 = (int(y_i == y_val) - (1 / num_classes)) / c_x
 
-                #A1
-                A1 = (int(y_i == y_val) / c_x) - c_plus / (c_x * (c_x - 1))
+                if c_x >= 2:
 
-                #A2
-                A2 = 0.0
-                for k in range(c):
-                    if c-k >= 0 and c_x >= 0:
-                        bin1 = comb(c - k, c_x)
-                        bin2 = comb(c + 1, c_x)
-                        term = 1/(k+1) - 1/(k+1) * (bin1 / bin2)
-                        A2 += term
-                A2 -= 1.0
+                    # Anzahl der Nachbarn von x_val in D-zi, mit gleiches Label haben
+                    y_neighbors = y_minus_i[neighbor_indices_minus_zi]
+                    c_plus = np.sum(y_neighbors == y_val)
 
-                #Berechnung des Shapley_wertes für zi
-                phi[i] = int(c_x >= 2) * A1 * A2 + (int(y_i == y_val) - ( 1 / num_classes)) / c_x
+                    # A1
+                    A1 = (int(y_i == y_val) / c_x) - c_plus / (c_x * (c_x - 1))
+
+                    # A2
+                    A2 = 0.0
+                    for k in range(c):
+                        if c - k >= 0 and c_x >= 0:
+                            bin1 = comb(c - k, c_x)
+                            bin2 = comb(c + 1, c_x)
+                            term1 = 1 / (k + 1) - 1 / (k + 1) * (bin1 / bin2)
+                            A2 += term1
+                    A2 -= 1.0
+                    # Berechnung des Shapley_wertes für zi
+                    phi[i] = int(c_x >= 2) * A1 * A2 + term2
+
+                else:
+                    phi[i] = term2
+
         return phi
 
 