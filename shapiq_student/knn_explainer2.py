a01278c70142b19d3bee38177bf20ab1101736b4
Andreas Lukas Schulz <Schulz.An@campus.lmu.de>
2025-06-20 17:20:05 +0200

    Code Überarbeitet und Notebook zum Testen vorbereitet

--- shapiq_student/knn_explainer2.py
+++ shapiq_student/knn_explainer2.py
@@ -1,7 +1,15 @@
 import numpy as np
 from shapiq import Explainer, InteractionValues
-from math import comp
 
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+def comb(n, k):
+    return factorial(n) // (factorial(k) * factorial(n - k))
 
 class KNNExplainer(Explainer):
     def __init__(
@@ -12,20 +20,6 @@ class KNNExplainer(Explainer):
         self.method = method
         self.labels = labels  # labels hinzugefügt für WKNN
 
-    def explain_function(self, x: np.ndarray, *args, **kwargs) -> InteractionValues:
-        if self.method == "standard_shapley":
-            interaction_values = self.knn_shapley(x)
-        elif self.method == "threshold":
-            threshold = kwargs["threshold"]
-            interaction_values = self.threshold_knn_shapley(x, threshold)
-        elif self.method == "weighted":
-            gamma = kwargs["gamma"]
-            interaction_values = self.weighted_knn_shapley(x, gamma)
-        else:
-            print('Method must be one of "standard_shapley", "threshold", "weighted"')
-
-        return interaction_values
-
     def knn_shapley(self, x_query):
         # TODO Implement knn shapley
         pass
@@ -34,12 +28,12 @@ class KNNExplainer(Explainer):
         # TODO Implement theshold
         pass
 
-    def weighted_knn_shapley(self, x_query, gamma, K):
+    def weighted_knn_shapley(self, x_train, y_train, x_test, y_test, gamma, K):
         # Implement weighted
         # if K = null ??
-        x_val, y_val = x_query
-        X = self.dataset
-        Y = self.labels
+        x_val, y_val = x_test, y_test
+        X = x_train
+        Y = y_train
         N = len(X)  # Menge der Daten im Datensatz
 
         # Berechnung der distanz
@@ -60,10 +54,11 @@ class KNNExplainer(Explainer):
 
         for i in range(1, N):
             b = 3 #Seite 8: Baselines & Settings & Hyperparameters/Seite 6 Remark 3
-            w_k = np.linspace(0, K, 2^b * K)
+            w_k = np.linspace(0, K, (2**b) * K)
 
             # Initialisierung von F als Dictionary
             F_i = {}
+            F_0 = {}
             # F als 0 setzen
             for m in range(1, N):
                 for length in range(1, K - 1):
@@ -73,49 +68,50 @@ class KNNExplainer(Explainer):
             for m in range(1, N):
                 if m == i:
                     continue
-                F_i = 1
+                for s in w_k:
+                    F_i[(m, 1, s)] = 1
 
             # Berechnung von F
             for length in range(2, K-1):
-                for t in range(1, length):
-                    F_0 = sum(F_i[(t, length - 1, s)])
+                F_0[length] = sum(F_i.get((t, length - 1, s), 0) for t in range(1, length -1)) 
                 for m in range(length, N):
                     if m == i:
                         continue
                     for s in w_k:
                         w_m = w_j[m]
-                        F_i[(m, length, s)] = F_0[(s - w_m)]
+                        F_i[(m, length, s)] = F_0.get((s - w_m), 0)
 
             # Berechnung von R_0
             R_0 = {}
             upper = max(i + 1, K + 1)
             for s in w_k:
-                R_0[s] = sum(F_i[(t, K - 1, s)] for t in range(1, upper - 1) if t != i)
+                R_0[s] = sum(F_i.get((t, K - 1, s), 0) for t in range(1, upper - 1) if t != i)
             #Berechnung von R_im
             for m in range(upper, N):
                 if Y_sorted == y_val:
                     R_im = sum(R_0[s] for s in range(- w_i, - w_m))
                 else:
                     R_im = sum(R_0[s] for s in range(- w_m, - w_i))
-                R_0 = R_0 + F_i[(m, K - 1, s)]
+                R_0 = R_0 + F_i.get((m, K - 1, s), 0)
 
             # Berechnung von G
             G_i0 = {}
-            if w_i < 0:
-                G_i0 = -1
-            else:
-                for length in range(1, K - 1):
-                    G_il = {}
-                    if Y_sorted == y_val:
-                        for m in range(N):
-                            if m == i:
-                                continue
-                            G_il = sum(F_i [(m, length, s)]) * sum(F_i [(m, length, s)] for s in range(-w_i, 0))
-                    else:
-                        for m in range(N):
-                            if m == i:
-                                continue
-                            G_il = sum(F_i [(m, length, s)]) * sum(F_i [(m, length, s)] for s in range(-w_i))
+            for count in range(0, len(w_i)):
+                if w_i[count] < 0:
+                    G_i0[count] = -1
+                else:
+                    for length in range(1, K - 1):
+                        G_il = {}
+                        if Y_sorted[length] == y_val:
+                            for m in range(N):
+                                if m == i:
+                                    continue
+                                G_il[length] = sum(F_i.get((m, length, s), 0)) * sum(F_i.get((m, length, s), 0) for s in range(-w_i[s], 0))
+                        else:
+                            for m in range(N):
+                                if m == i:
+                                    continue
+                                G_il[m] = sum(F_i.get((m, length, s), 0)) * sum(F_i.get((m, length, s), 0) for s in range(-w_i, 0))
 
             # Berechnung des Shapleys von shapley Values
             phi = 0
@@ -129,12 +125,12 @@ class KNNExplainer(Explainer):
 
             first_term = 0
             for length in range(K):
-                first_term += G_il / comp(N-1, length)
+                first_term += G_il / comb(N-1, length)
             first_term = (1 / N) * first_term
 
             second_term = 0
             for m in range(max(i + 1, K + 1), N + 1):
-                second_term += R_im / m * comp(m - 1, K)
+                second_term += R_im / m * comb(m - 1, K)
 
             phi = sign * (first_term + second_term)
         return phi
