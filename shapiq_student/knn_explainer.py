9e6e0e5857a21613cb9634fdc774d89419923165
phamal <phamal@cip.ifi.lmu.de>
2025-07-14 21:53:33 +0200

    Changed KNNExplainer and removed unnecessary parameters in threshold.py

--- shapiq_student/knn_explainer.py
+++ shapiq_student/knn_explainer.py
@@ -4,7 +4,6 @@ import numpy as np
 from networkx import neighbors
 from shapiq import Explainer, InteractionValues
 
-from shapiq_student.knn_shapley import KNNShapley
 from shapiq_student.threshold import Threshold
 
 
@@ -19,53 +18,28 @@ class KNNExplainer(Explainer):
         index = "SV",
         random_state = 42):
             super().__init__(model, data, class_index, max_order=max_order, index = index)
-            self.model = model
             self.dataset = data
             self.labels = labels
-            self.class_index = class_index
             self.model_name = model_name
             self.N, self.M = data.shape
-            self.threshold = Threshold(model, data, labels, class_index)
-            self.knn_shapley = KNNShapley(model, data, labels, class_index)
             self.random_state = np.random.RandomState(random_state)
 
             if hasattr(model, 'weights') and model.weights == 'distance':
                 self.mode = 'weighted'
             elif hasattr(model, 'radius') and model.radius is not None:
                 self.mode = 'threshold'
+                self.threshold = Threshold(model, data, labels, class_index, model.radius)
             else:
                 self.mode = 'normal'
 
-    # def explain_function(self, x: np.ndarray, *args: Any, **kwargs: Any) -> InteractionValues:
-    #     radius = kwargs.get("radius")
-    #     gamma = kwargs.get("gamma")
-    #     if radius is not None and radius > 0:
-    #         threshold = radius
-    #         x_query = kwargs["x_query"]
-    #         num_classes = kwargs["num_classes"]
-    #         values = self.threshold.threshold_knn_shapley(x_query, threshold, num_classes)
-    #     elif gamma is not None:
-    #         values = self.weighted_knn_shapley(x, gamma)
-    #     else:
-    #         values = self.knn_shapley(x)
-    #     pass
-
-
-    def explain(self, x: np.ndarray | None = None, *args, **kwargs) -> InteractionValues:
-        radius = kwargs.get("radius")
+    def explain(self, x: np.ndarray, *args, **kwargs) -> InteractionValues:
         gamma = kwargs.get("gamma")
-        if radius is not None and radius > 0:
-            threshold = radius
-            x_query = kwargs["x_query"]
-            num_classes = kwargs["num_classes"]
-            shapley_values = self.threshold.threshold_knn_shapley(x_query, threshold, num_classes)
-            print("shapley_values:", shapley_values)
-            print(threshold, radius)
-            print(x_query, num_classes)
-        elif gamma is not None:
+        if self.mode == "threshold":
+            shapley_values = self.threshold.threshold_knn_shapley(x)
+        elif self.mode == 'weighted':
             shapley_values = self.weighted_knn_shapley(x, gamma)
         else:
-            shapley_values = self.knn_shapley.knn_shapley(x)
+            shapley_values = self.knn_shapley(x)
 
         n_features = x.shape[1] if len(x.shape) > 1 else x.shape[0]
         print(n_features)
