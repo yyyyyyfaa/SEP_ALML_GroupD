f7169cb4443889399a4371c92ade774299bc8714
li <22222222>
2025-07-17 17:25:59 +0200

    gc_imputer sec_commit

---
+++ shapiq_student/imputers/gaussian_conditional_imputer.py
@@ -0,0 +1,51 @@
+import numpy as np
+from numpy.linalg import inv
+from shapiq.games.imputer.base import Imputer
+
+class GaussianConditionalImputer(Imputer):
+    def fit(self, X: np.ndarray, mask: np.ndarray = None):
+        # 1. only use the samples without any missing values to estimate
+        full_rows = ~mask.any(axis=1)
+        X_full = X[full_rows]
+        self.mean = X_full.mean(axis=0)
+        # every row only 1 sample and use no bias estimate
+        self.CovMatrix = np.cov(X_full, rowvar=False, bias=False)
+        return self
+
+    def transform(self, X: np.ndarray, mask: np.ndarray = None) -> np.ndarray:
+        X_imp = X.copy()
+        n_samples, n_features = X.shape
+
+        for i in range(n_samples):
+            # find out the missing value
+            missing_idx = np.where(mask[i])[0]
+            if missing_idx.size == 0:
+                continue
+            observed_idx = np.where(~mask[i])[0]
+
+            # 2.  submatrix accroding to "Multivariate Gaussian distribution"
+            mu = self.mean
+            Σ = self.CovMatrix
+            Σ_CC = Σ[np.ix_(observed_idx, observed_idx)]
+            Σ_MC = Σ[np.ix_(missing_idx,   observed_idx)]
+            Σ_MM = Σ[np.ix_(missing_idx,   missing_idx)]
+
+            # 3. compute mean value
+            x_C = X_imp[i, observed_idx]
+            mu_C = mu[observed_idx]
+            mu_M = mu[missing_idx]
+            Σ_CC_inv = inv(Σ_CC)  # 确保 Σ_CC 可逆；有需要可加微小正则项
+            cond_mean = mu_M + Σ_MC.dot(Σ_CC_inv.dot(x_C - mu_C))
+
+            # 4. compute covariance
+            cond_cov = Σ_MM - Σ_MC.dot(Σ_CC_inv.dot(Σ_MC.T))
+
+            # 5. use mean value to fill missing value
+            X_imp[i, missing_idx] = cond_mean
+
+        return X_imp
+
+
+
+
+
