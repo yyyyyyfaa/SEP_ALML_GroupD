99cafaeb6ab9e12a00a4a83b813fa3d94c7f901a
li <22222222>
2025-07-18 10:53:10 +0200

    "gc_imputer build and tested"

--- shapiq_student/gaussian_conditional_imputer.py
+++ shapiq_student/gaussian_conditional_imputer.py
@@ -4,6 +4,9 @@ from shapiq.games.imputer.base import Imputer
 
 class GaussianConditionalImputer(Imputer):
     def fit(self, X: np.ndarray, mask: np.ndarray = None):
+        # Handle default mask
+        if mask is None:
+            mask = np.isnan(X)
         # 1. only use the samples without any missing values to estimate
         full_rows = ~mask.any(axis=1)
         X_full = X[full_rows]
@@ -14,7 +17,11 @@ class GaussianConditionalImputer(Imputer):
 
     def transform(self, X: np.ndarray, mask: np.ndarray = None) -> np.ndarray:
         X_imp = X.copy()
+        # Handle default mask
+        if mask is None:
+            mask = np.isnan(X_imp)
         n_samples, n_features = X.shape
+        eps = 1e-6
 
         for i in range(n_samples):
             # find out the missing value
@@ -25,25 +32,27 @@ class GaussianConditionalImputer(Imputer):
 
             # 2.  submatrix accroding to "Multivariate Gaussian distribution"
             mu = self.mean
-            Σ = self.CovMatrix
-            Σ_CC = Σ[np.ix_(observed_idx, observed_idx)]
-            Σ_MC = Σ[np.ix_(missing_idx,   observed_idx)]
-            Σ_MM = Σ[np.ix_(missing_idx,   missing_idx)]
+            Sigma = self.CovMatrix
+            Sigma_CC = Sigma[np.ix_(observed_idx, observed_idx)]
+            Sigma_MC = Sigma[np.ix_(missing_idx,   observed_idx)]
+            Sigma_MM = Sigma[np.ix_(missing_idx,   missing_idx)]
 
             # 3. compute mean value
             x_C = X_imp[i, observed_idx]
             mu_C = mu[observed_idx]
             mu_M = mu[missing_idx]
-            Σ_CC_inv = inv(Σ_CC)  # 确保 Σ_CC 可逆；有需要可加微小正则项
-            cond_mean = mu_M + Σ_MC.dot(Σ_CC_inv.dot(x_C - mu_C))
+            Sigma_CC_inv = inv(Sigma_CC + eps * np.eye(Sigma_CC.shape[0]))  # 确保 Σ_CC 可逆
+            cond_mean = mu_M + Sigma_MC.dot(Sigma_CC_inv.dot(x_C - mu_C))
 
             # 4. compute covariance
-            cond_cov = Σ_MM - Σ_MC.dot(Σ_CC_inv.dot(Σ_MC.T))
+            cond_cov = Sigma_MM - Sigma_MC.dot(Sigma_CC_inv.dot(Sigma_MC.T))
 
             # 5. use mean value to fill missing value
-            X_imp[i, missing_idx] = cond_mean
+            sampled = np.random.multivariate_normal(cond_mean, cond_cov)
+            X_imp[i, missing_idx] = sampled
 
         return X_imp
+
 
 
 
