ea5fefc25bf5ba1291ea4fd4cf273e321912083c
Tobias Bechtold <T.Bechtold@campus.lmu.de>
2025-07-20 13:44:56 +0200

    deleted src folder

--- src/src/shapiq_student/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Source code for the shapiq_student package."""
-from .knn_explainer import KNNExplainer
-from .knn_shapley import KNNShapley
-from .threshold import Threshold
-from .wknn_explainer import Weighted
