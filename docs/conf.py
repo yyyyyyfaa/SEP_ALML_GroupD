30e8ef3531b6d36aea9d0eadd26176277dbc7a8a
Tobias Bechtold <T.Bechtold@campus.lmu.de>
2025-07-20 14:42:12 +0200

    sys path changed

--- docs/conf.py
+++ docs/conf.py
@@ -9,8 +9,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-sys.path.insert(0, os.path.abspath("../examples"))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 project = 'SEP Game Theoretic Explainable AI GroupD'
 copyright = '2025, Alina, Andreas, Maren, Tobias, Yifan'
