d335dc638c9e99896e108f796f484b75cdb7ccb5
Tobias Bechtold <T.Bechtold@campus.lmu.de>
2025-07-20 22:52:15 +0200

    ruff and renaming

--- shapiq_student/__init__.py
+++ shapiq_student/__init__.py
@@ -1,14 +1,13 @@
 """Source code for the shapiq_student package."""
 
 
+from .gaussian_copula_imputer import GaussianCopulaImputer
 from .gaussian_imputer import GaussianImputer
-from .gaussion_copula_imputer import GaussianCopulaImputer
 from .knn_explainer import KNNExplainer
 from .knn_shapley import KNNShapley
+from .subset_finding import subset_finding
 from .threshold import Threshold
 from .weighted import Weighted
-from .subset_finding import subset_finding
-
 
 __all__ = ["GaussianCopulaImputer", "GaussianImputer", "KNNExplainer", "KNNShapley", "Threshold", "Weighted", "subset_finding"]
 
